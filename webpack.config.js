4bcf3e1ddaf212d2c284a7228263021b1fc8ed32
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-14 13:26:22 -0600

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+    mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
-
     rules: [
-
       {
-
         test: /\.css$/i,
-
         use: ['style-loader', 'css-loader'],
-
       },
-
     ],
-
   },
 };
