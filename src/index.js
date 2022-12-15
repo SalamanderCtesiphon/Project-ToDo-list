47b196c98ad6d6dabb2348ff1671e46fc89c9a88
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-15 11:10:15 -0600

    created the basic divs to layout the page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,27 @@ import './style.css';
 import printMe from './print';
 
 function component() {
+    const content = document.createElement('div');
+    const header = document.createElement('div');
+    const sidebar = document.createElement('div');
+    const body = document.createElement('div');
+    const footer = document.createElement('div');
     
+    content.classList.add('content');
+    header.classList.add('header');
+    sidebar.classList.add('sidebar');
+    body.classList.add('body');
+    footer.classList.add('footer');
+
+    content.appendChild(header);
+    content.appendChild(sidebar);
+    content.appendChild(body);
+    content.appendChild(footer);
+
+    header.textContent = 'To Do List';
+    sidebar.textContent = 'Projects';
+
+    document.body.appendChild(content);
   }
   
-  document.body.appendChild(component());
+  component();
