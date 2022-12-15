b132c6fe25cb8aa7e0ddcfe5400b7be339b2d641
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-15 11:58:43 -0600

    add two buttons. one to create new project the other to create new todo

--- src/index.js
+++ src/index.js
@@ -8,12 +8,20 @@ function component() {
     const sidebar = document.createElement('div');
     const body = document.createElement('div');
     const footer = document.createElement('div');
+    const projectsBtn = document.createElement('button');
+    const toDoBtn = document.createElement('button');
+
     
     content.classList.add('content');
     header.classList.add('header');
     sidebar.classList.add('sidebar');
     body.classList.add('body');
     footer.classList.add('footer');
+    projectsBtn.classList.add('projectsBtn');
+    toDoBtn.classList.add('toDoBtn');
+
+    sidebar.appendChild(projectsBtn);
+    body.appendChild(toDoBtn);
 
     content.appendChild(header);
     content.appendChild(sidebar);
@@ -23,6 +31,9 @@ function component() {
     header.textContent = 'To Do List';
     sidebar.textContent = 'Projects';
     footer.textContent = `SeamusQuixote 2022`;
+    projectsBtn.textContent = 'Add New Project';
+    toDoBtn.textContent = 'Add New To Do';
+    
 
     document.body.appendChild(content);
   }
