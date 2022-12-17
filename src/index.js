fc83b9ee745b7fcde9465d8c2f360ef6908481f6
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-17 11:20:14 -0600

    still adding functionality to the project objects

--- src/index.js
+++ src/index.js
@@ -12,29 +12,31 @@ class Project {
   }
 }
 
-document.querySelector('.projectsBtn').addEventListener('click', () => {
+document.querySelector('.projectsBtn').addEventListener('click', createProject);
+
+function createProject() {
   const projectName = prompt('Enter a project name');
   const newProject = new Project(projectName);
   const sidebar = document.querySelector('.sidebar');
   const ul = document.createElement('ul');
   const li = document.createElement('li');
   const button = document.createElement('button');
+  const selectProject = document.createElement('button');
+  const body = document.querySelector('.body');
 
+  selectProject.textContent = 'Select Project';
   projectList.push(newProject);
   sidebar.appendChild(ul);
   ul.appendChild(li);
   li.textContent = newProject.name;
   li.appendChild(button);
+  li.appendChild(selectProject);
   button.textContent = 'Delete Project';
   button.addEventListener('click', () => {
     ul.removeChild(li);
     projectList.splice(projectList.indexOf(newProject), 1);
   });
-
-  console.log(projectList);
-});
-
-
+}
 
 
 class ToDo {
