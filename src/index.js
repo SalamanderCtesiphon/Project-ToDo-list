e0afff1f281a331966e47aa6ea99eae9675daa2a
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-17 10:56:59 -0600

    added a project class and a to do class to the project

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import { component } from './loadPage';
 
   component();
   
+
+  class Project {
+    constructor(name) {
+      this.name = name;
+      this.toDoList = [];
+    }
+  }
+
+  class ToDo {
+    constructor(name, dueDate, priority, notes) {
+      this.name = name;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.notes = notes;
+    }
+  }
+
+  const projectList = [];
